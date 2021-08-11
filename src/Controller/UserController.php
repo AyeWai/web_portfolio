6ac1077fcfb6ccbf7f054e144c0b322ae7d38a53
Chris <cs.simon@live.fr>
2021-08-11 06:19:02 +0200

    Mailer from ContactService  effective

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Service\ContactService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -62,38 +64,14 @@ class UserController extends AbstractController
      * @Route("/user-resgistered", name="create_user2")
      */
 
-     public function createUser2(Request $request, ValidatorInterface $validator) : Response
+     public function createUser2(Request $request, ValidatorInterface $validator, ContactService $contactService, MailerInterface $mailer) : Response
      {
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $firstname = $request->request->get("firstName");
-        $lastname = $request->request->get("lastName");
-        $mail = $request->request->get("email");
-        $status = $request->request->get("status");
-
-        $user = new User();
-        $user->setFirstname($firstname);
-        $user->setLastname($lastname);
-        $user->setMail($mail);
-        $user->setStatus($status);
-
-        $errors = $validator->validate($user);
-        if (count($errors) > 0) {
-            //return new Response((string) $errors, 400);
-            return $this->render('home/errors.html.twig', ['errors' => $errors,]);
-        }
-        elseif(count($errors) == 0){
-
-
-            // tell Doctrine you want to (eventually) save the Product (no queries yet)
-            $entityManager->persist($user);
-
-            // actually executes the queries (i.e. the INSERT query)
-            $entityManager->flush();
+        $contactService->persistContact($request, $validator);
+        $contactService->sendEmail($mailer, $request);
         
         return $this->render('user/new.html.twig');
         //return new Response('Saved new contact user with id '.$user->getId());
-        }
+        
      }
 
     public function contact(Request $request): Response
--- src/Service/ContactService.php
+++ src/Service/ContactService.php
@@ -2,7 +2,61 @@
 
 namespace App\Service;
 
-class ContactService{
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-    public function persistContact()
+class ContactService extends AbstractController{
+
+    public function persistContact(Request $request, ValidatorInterface $validator){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $firstname = $request->request->get("firstName");
+        $lastname = $request->request->get("lastName");
+        $mail = $request->request->get("email");
+        $status = $request->request->get("status");
+
+        $user = new User();
+        $user->setFirstname($firstname);
+        $user->setLastname($lastname);
+        $user->setMail($mail);
+        $user->setStatus($status);
+
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            //return new Response((string) $errors, 400);
+            return $this->render('home/errors.html.twig', ['errors' => $errors,]);
+        }
+        elseif(count($errors) == 0){
+
+
+            // tell Doctrine you want to (eventually) save the Product (no queries yet)
+            $entityManager->persist($user);
+
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();
+        }
+    }
+
+    public function sendEmail(MailerInterface $mailer, Request $request ): void
+    {
+        $email = (new Email())
+            ->from('cs.simon@live.fr')
+            ->to('cs.simon@live.fr')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        // ...
+    }
 }
